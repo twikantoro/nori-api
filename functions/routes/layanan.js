f40cfcfbbfcb4b9dc1fe3929cb379b6f5c6778bd
twikantoro <trisnoadi.wikantoro@gmail.com>
2020-04-23 16:29:14 +0700

    renamed layanan to klaster

--- functions/routes/layanan.js
+++ functions/routes/layanan.js
@@ -3,30 +3,26 @@ var router = express.Router();
 var admin = require('../config/firebaseAdminConfig')
 var db = admin.firestore()
 
-router.get('/getAllByKodeOLD', async function (req, res, next) {
-  //step1: get id
-  var id_gerai = await db.collection('gerai').where('kode', '==', req.query.kode).get().then(docs => {
-    if (docs.empty) return false
-    docs.forEach(doc => {
-      return doc.id
-    })
+router.get('/create', async function (req, res, next) {
+  //step1: verify ownership
+  var step1 = await amITheOwner(req.query)
+  if (!step1) {
+    res.send("bukan milikmu"); return
+  }
+  //step2: create
+  var data = {
+    id_klaster: req.query.id_klaster,
+    nama: req.query.nama,
+    kode: req.query.kode,
+    deskripsi: req.query.deskripsi,
+    syarat: req.query.syarat,
+    durasi: req.query.durasi
+  }
+  db.collection('layanan').doc().set(data).then(response => {
+    res.send("sukses")
+  }).catch(e=>{
+    res.send(e)
   })
-  if (!id_gerai) { res.send(false); return 0 }
-  //step2: get layanans
-  var layanans = await db.collection('layanan').where('id_gerai', '==', id_gerai).get().then(docs => {
-    if (docs.empty) return false
-    var data
-    var i = 0
-    docs.forEach(doc => {
-      data[i] = doc.data()
-      data[i].id = doc.id
-      i++
-    })
-    return data
-  })
-  if (!layanans) { res.send(false); return 0 }
-  // final step
-  res.send(layanans)
 })
 
 async function amITheOwner(query) {
@@ -52,94 +48,5 @@ async function amITheOwner(query) {
     resolve(step2)
   })
 }
-
-router.get("/tesjancuk", async function (req, res, next) {
-  var jancuk = await db.collection('gerai').where('kode', '==', 'oke').get().then(response => {
-    return response
-  })
-  res.send(jancuk)
-})
-
-router.get('/create', async function (req, res, next) {
-  //step1 verify ownership
-  var step1 = await amITheOwner(req.query)
-  if (!step1) {
-    res.send("bukan milikmu")
-    return
-  }
-  //step2 get the gerai id
-  var step2 = await db.collection('gerai').where('kode', '==', req.query.kode).get().then(response => {
-    if (response.empty) {
-      return false
-    } else {
-      var shit = ""
-      response.forEach(doc => {
-        shit = doc.id
-      })
-      return shit
-    }
-  })
-  if (!step2) {
-    res.send("unable to find gerai id")
-    return
-  }
-  //step3 execute create
-  var params = {
-    id_gerai: step2,
-    nama: req.query.nama,
-    jadwal: req.query.jadwal,
-    kode: req.query.kode
-  }
-  var step3 = await db.collection('layanan').doc().set(params).then(response => {
-    res.send("sukses")
-  }).catch(e => {
-    res.send(e)
-  })
-})
-
-router.get('/getAllByKode', async function (req, res, next) {
-  //step1 verify ownership
-  var step1 = await amITheOwner(req.query)
-  if (!step1) {
-    res.send("bukan milikmu")
-    return
-  }
-  //step2 get id gerai
-  var step2 = await db.collection('gerai').where('kode', '==', req.query.kode).get().then(response => {
-    if (response.empty) {
-      return false
-    } else {
-      var id = ""
-      response.forEach(doc => {
-        id = doc.id
-      })
-      return id
-    }
-  })
-  if (!step2) {
-    res.send("unable to find gerai id")
-    return
-  }
-  //step3 get all layanans by id gerai
-  var step3 = await db.collection('layanan').where('id_gerai', '==', step2).get().then(response => {
-    if (response.empty) {
-      return false
-    } else {
-      var layanans = new Array()
-      var i = 0
-      response.forEach(doc => {
-        layanans[i] = doc.data()
-        layanans[i].id = doc.id
-        i++
-      })
-      return layanans
-    }
-  })
-  if (!step3) {
-    res.send("no layanans found")
-  } else {
-    res.send(step3)
-  }
-})
 
 module.exports = router;
